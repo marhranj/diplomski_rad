50fb64bf4aea733034bb3c61a6bf1cbc9a1d581f
Marijan Hranj <marijan.hranj@ecx.io>
2020-06-16 15:32:55 +0200

    first commit

---
+++ src/main/kotlin/hranj/marijan/diplomskirad/DiplomskiRadApplication.kt
@@ -0,0 +1,11 @@
+package hranj.marijan.diplomskirad
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DiplomskiRadApplication
+
+fun main(args: Array<String>) {
+	runApplication<DiplomskiRadApplication>(*args)
+}
